a0ad5f922fa34343d2d5b743d63e6b706e7a2404
Renan Ciciliato <renanciciliato@gmail.com>
2020-01-31 11:47:13 -0200

    Aula operações em arrays

--- main.js
+++ main.js
@@ -62,3 +62,26 @@ function teste(x) {
 }
 // console.log(y); // erro, fora escopo
 teste(10);
+
+// arrays
+const arr = [1, 3, 4, 5, 8, 9];
+
+const newArr = arr.map(function(item, index){
+  return item + index;
+});
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next) {
+  return total + next;
+});
+console.log(sum);
+
+const filter = arr.filter(function(item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+
+const find = arr.find(function(item) {
+  return item === 4;
+});
+console.log(find);
