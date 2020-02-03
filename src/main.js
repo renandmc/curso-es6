1ecf72a7db0a35cb6ca7d6eec9841a4b66f9a6ef
Renan Ciciliato <renanciciliato@gmail.com>
2020-02-03 10:26:00 -0200

    Aula Buscando da API

--- src/main.js
+++ src/main.js
@@ -242,13 +242,16 @@ class Api {
 
 Api.getUserInfo('renandmc');
 
-// adicionar repositórios
+// adicionar repositórios / render em tela / buscando da API
+import api from './api';
+
 class App {
 
   constructor() {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.inputEl = document.querySelector('input[name=repository]');
     this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers(); 
@@ -258,18 +261,23 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
 
-    this.repositories.push({
-      name: 'rocketseat.com.br',
-      description: 'Tire a sua ideia do papel e dê vida à sua startup',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'https://github.com/rocketseat/awesome'
-    });
+    const repoInput = this.inputEl.value;
 
-    console.log(this.repositories);
+    if (repoInput.length === 0) {
+      return;
+    }
 
+    const response = await api.get(`/repos/${repoInput}`);
+
+    const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+    this.repositories.push({ name, description, avatar_url, html_url });
+
+    this.inputEl.value = '';
+    
     this.render();
   }
 
